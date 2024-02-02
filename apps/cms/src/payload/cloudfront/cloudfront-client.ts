317838e7ca98d4c030681cf671c9a42be09d1b85
Bence Molnár <bence.molnar@accretivetg.com>
2024-02-02 22:12:32 +0100

    add state & env columns

--- apps/cms/src/payload/cloudfront/cloudfront-client.ts
+++ apps/cms/src/payload/cloudfront/cloudfront-client.ts
@@ -5,7 +5,7 @@ const projectName = process.env.CLOUDFLARE_PROJECT_NAME;
 
 function fetch(
   input: RequestInfo | URL,
-  init?: RequestInit,
+  init?: RequestInit
 ): Promise<Response> {
   const authHeader = {
     Authorization: `Bearer ${process.env.CLOUDFLARE_PAGES_TOKEN}`,
@@ -23,17 +23,26 @@ export type Deployment = {
   created_on: string;
   latest_stage: {
     name: 'queued' | 'initialize' | 'clone_repo' | 'build' | 'deploy';
-    status: 'active' | 'success' | 'failure';
+    status: 'idle' | 'active' | 'success' | 'failure';
   };
 };
 
 export async function getDeployments(): Promise<Deployment[]> {
   try {
     const res = await fetch(
-      `accounts/${accountId}/pages/projects/${projectName}/deployments`,
+      `accounts/${accountId}/pages/projects/${projectName}/deployments`
     );
     const body = await res.json();
-    return body.result;
+    return body.result.map(
+      ({ id, environment, url, created_on, latest_stage }) =>
+        ({
+          id,
+          environment,
+          url,
+          created_on,
+          latest_stage,
+        } satisfies Deployment)
+    );
   } catch (err) {
     payload.logger.error('Error fetching Cloudflare deployments', err);
   }
@@ -44,7 +53,7 @@ export async function createDeployment(): Promise<Deployment> {
   try {
     const res = await fetch(
       `accounts/${accountId}/pages/projects/${projectName}/deployments`,
-      { method: 'POST' },
+      { method: 'POST' }
     );
     const body = await res.json();
     return body.result;
--- apps/cms/src/payload/components/Deployments/Deployments.tsx
+++ apps/cms/src/payload/components/Deployments/Deployments.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import useSWR from 'swr';
 import { Deployment } from '../../cloudfront/cloudfront-client';
 
+import { Pill } from 'payload/components';
 import './deployments.scss';
 
 const useDeployments = () => {
@@ -16,12 +17,15 @@ const useDeployments = () => {
   const baseURL = `${serverURL}${api}`;
   const { data, isLoading, isValidating, error } = useSWR<Deployment[]>(
     `${baseURL}/deployments`,
-    () => fetch(`${baseURL}/deployments`).then((res) => res.json())
-    // { refreshInterval: 10 * 1000 }
+    () => fetch(`${baseURL}/deployments`).then((res) => res.json()),
+    {
+      suspense: true,
+      // refreshInterval: 10 * 1000, //TODO enable this
+    }
   );
 
   return {
-    deployments: data,
+    data,
     isLoading,
     isValidating,
     error,
@@ -30,12 +34,40 @@ const useDeployments = () => {
 
 const deploymentColumns: Column[] = [
   {
+    name: 'Environment',
+    accessor: 'environment',
+    active: true,
+    components: {
+      Heading: <div>Environment</div>,
+      renderCell: (row, data) => (
+        <Pill pillStyle="light-gray" rounded>
+          {data}
+        </Pill>
+      ),
+    },
+    label: 'State',
+  },
+  {
+    name: 'State',
+    accessor: 'latest_stage',
+    active: true,
+    components: {
+      Heading: <div>State</div>,
+      renderCell: (row, data) => (
+        <Pill pillStyle="success">
+          {data.name}:{data.status}
+        </Pill>
+      ),
+    },
+    label: 'State',
+  },
+  {
     name: 'Created on',
     accessor: 'created_on',
     active: true,
     components: {
       Heading: <div>Created on</div>,
-      renderCell: (row, data) => formatDate(data, 'yyyy.MM.dd. HH:mm:ss'),
+      renderCell: (row, data) => formatDate(data, 'yyyy.MM.dd. HH:mm:ss'), //TODO extract this & use date format from payload config
     },
     label: 'Created on',
   },
@@ -44,7 +76,7 @@ const deploymentColumns: Column[] = [
 const baseClass = 'deployments';
 
 const Deployments: React.FC = () => {
-  const { deployments } = useDeployments();
+  const { data: deployments } = useDeployments();
   return (
     <div className={baseClass}>
       <Table columns={deploymentColumns} data={deployments} />
